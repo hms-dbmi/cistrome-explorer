cd0adda5df14906de930316df1b7d4eca0dfb17e
Sehi L'Yi <shlyi@hcil.snu.ac.kr>
2020-02-05 17:05:47 -0500

    Added an option to specify the display name for url (#32)
    * Added an option to specify the display name for url
    
    * Fixed the url

--- src/TrackWrapper.js
+++ src/TrackWrapper.js
@@ -69,6 +69,7 @@ export default function TrackWrapper(props) {
                     trackHeight={trackHeight}
                     trackWidth={trackWidth}
                     rowLinkAttribute={options.rowLinkAttribute}
+                    rowLinkNameAttribute={options.rowLinkNameAttribute}
                     rowLinkPosition={options.rowLinkPosition}
                 />) : null}
             {(!combinedTrack && options.colToolsPosition !== "hidden") ? 
