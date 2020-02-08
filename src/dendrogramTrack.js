b6167c647474aa2bc8b34bc8052114ad6bab19ef
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2020-02-07 19:23:50 -0500

    Update filename capitalization

---
+++ src/dendrogramTrack.js
@@ -0,0 +1,66 @@
+import range from 'lodash/range';
+import d3 from './utils/d3.js';
+import { matrixToTree } from './utils/tree.js';
+
+/**
+ * Dendrogram for rendering an attribute of hierarchical structure.
+ * @prop {object} two An instance of the Two class.
+ * @prop {number} left The left position of this view.
+ * @prop {number} top The top position of this view.
+ * @prop {number} width The width of this view.
+ * @prop {number} height The height of this view.
+ * @prop {array} rowInfo Array of JSON objects, one object for each row.
+ * @prop {object} attribute The name and type of data field.
+ * @prop {boolean} isLeft Is this view on the left side of the track?
+ */
+export function dendrogramTrack(props) {
+    const {
+        two,
+        left, top, width, height,
+        rowInfo, 
+        attribute,
+        isLeft
+    } = props;
+
+    // Data, layouts and styles
+    const { name: field } = attribute;
+    const hierarchyData = matrixToTree(rowInfo.map(d => d[field]));
+    const root = d3.hierarchy(hierarchyData);
+
+    const treeLayout = d3.cluster()
+        .size([height, width])
+        .separation(() => 1);
+    treeLayout(root);
+
+    const descendants = root.descendants();
+
+    let pathFunction;
+    if(isLeft){
+        pathFunction = (d) => {
+            return two.makePath(
+                left + d.parent.y, top + d.parent.x,
+                left + d.parent.y, top + d.x,
+                left + d.y, top + d.x,
+                left + d.parent.y, top + d.x
+            );
+        }
+    } else {
+        pathFunction = (d) => {
+            return two.makePath(
+                left + width -  d.parent.y, top + d.parent.x,
+                left + width - d.parent.y, top + d.x,
+                left + width - d.y, top + d.x,
+                left + width - d.parent.y, top + d.x
+            );
+        }
+    }
+
+    descendants.forEach((d, i) => {
+        if(i > 0) {
+            const path = pathFunction(d);
+            path.stroke = "#555";
+            path.opacity = 0.6;
+            path.linewidth = 1.5;
+        }
+    });
+}
---
+++ src/verticalBarTrack.js
@@ -0,0 +1,88 @@
+import range from 'lodash/range';
+import d3 from './utils/d3.js';
+
+/**
+ * Bar chart for rendering an attribute.
+ * @prop {object} two An instance of the Two class.
+ * @prop {number} left The left position of this view.
+ * @prop {number} top The top position of this view.
+ * @prop {number} width The width of this view.
+ * @prop {number} height The height of this view.
+ * @prop {array} rowInfo Array of JSON objects, one object for each row.
+ * @prop {object} attribute The name and type of data field.
+ * @prop {boolean} isLeft Is this view on the left side of the track?
+ */
+export function verticalBarTrack(props) {
+    const {
+        two,
+        left, top, width, height,
+        rowInfo, 
+        attribute,
+        isLeft
+    } = props;
+
+    // Data, layouts and styles
+    const { name: field, type: fieldType } = attribute;
+    const isNominal = fieldType === "nominal";
+    const barAreaWidth = isNominal ? 20 : width - 20;
+    const textAreaWidth = isNominal ? 50 : 20;
+    const margin = 5;
+    const fontSize = 10;
+    
+    // Scales
+    const valueExtent = [0, d3.extent(rowInfo.map(d => d[field]))[1]];   // Zero baseline
+    const yScale = d3.scaleBand()
+        .domain(range(rowInfo.length))
+        .range([0, height]);
+    const xScale = d3.scaleLinear()
+        .domain(valueExtent)
+        .range([0, barAreaWidth])
+    const colorScale = isNominal ? 
+        d3.scaleOrdinal()
+            .domain(Array.from(new Set(rowInfo.map(d => d[field]))))
+            .range(d3.schemeSet3) : 
+        d3.scaleLinear()
+            .domain(valueExtent)
+            .range([0, 1]);
+    const rowHeight = yScale.bandwidth();
+
+    // Render visual components for each row (i.e., bars and texts).
+    const textAlign = isLeft ? "end" : "start";
+    let aggregateStartIdx = -1, sameCategoriesNearby = 1;
+
+    rowInfo.forEach((d, i) => {
+
+        // To aggregate bars, check if there is a same category on the next row.
+        if(i + 1 < rowInfo.length && d[field] === rowInfo[i+1][field]) {
+            if(aggregateStartIdx === -1) {
+                aggregateStartIdx = i;
+            }
+            sameCategoriesNearby++;
+            return;
+        }
+
+        const barTop = aggregateStartIdx !== -1 ? yScale(aggregateStartIdx) : yScale(i);
+        const barHeight = rowHeight * sameCategoriesNearby;
+        const barWidth = isNominal ? barAreaWidth : xScale(d[field]);        
+        const barLeft = left + (isLeft ? width - barWidth : 0);
+        const textLeft = left + (isLeft ? width - barWidth - margin : barWidth + margin);
+        const color = isNominal ? colorScale(d[field]) : 
+            d3.interpolateViridis(colorScale(d[field]));
+
+        const rect = two.makeRect(barLeft, barTop, barWidth, barHeight);
+        rect.fill = color;
+
+        // Render text labels when the space is enough.
+        if(barHeight >= fontSize){
+            const text = two.makeText(textLeft, barTop + barHeight/2, textAreaWidth, barHeight, d[field]);
+            text.fill = d3.hsl(color).darker(3);
+            text.fontsize = fontSize;
+            text.align = textAlign;
+            text.baseline = "middle";
+            text.overflow = "ellipsis";
+        }
+
+        aggregateStartIdx = -1;
+        sameCategoriesNearby = 1;
+    });
+}
