9192642c604d6eb6d428c9ef0f512fa364f7f640
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-02-26 13:16:25 -0500

    Revert "WIP: component test"
    This reverts commit 34e746b3bccc4e9b2aad5a39802a3f82654f1687.

--- babel.config.js
+++ babel.config.js
@@ -2,34 +2,19 @@ module.exports = {
     env: {
         test: {
             presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        modules: "commonjs"
-                    }
-                ],
+                ["@babel/preset-env", { modules: "commonjs" }],
                 "@babel/preset-react"
             ]
         },
         production: {
             presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        modules: false
-                    }
-                ],
+                ["@babel/preset-env", { modules: false }],
                 "@babel/preset-react"
             ]
         },
         development: {
             presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        modules: false
-                    }
-                ],
+                ["@babel/preset-env", { modules: false }],
                 "@babel/preset-react"
             ]
         }
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
     verbose: true,
-    moduleFileExtensions: [ "js" ],
-    transformIgnorePatterns: [
-        "/node_modules/(?!higlass-register|higlass-multivec).+\\.js$"
-    ],
-    transform: {
-        "^.+\\.js$": "babel-jest"
-    },
-    moduleNameMapper: {
-        "\\.(css|scss)$": "identity-obj-proxy"
-    },
+    moduleFileExtensions: [ "js" ]
 };
