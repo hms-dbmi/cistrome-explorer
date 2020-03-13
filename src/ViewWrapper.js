0677559d549a670f01b686af33ecced9180764d2
Sehi L'Yi <shlyi@hcil.snu.ac.kr>
2020-03-13 10:29:51 -0400

    Remove unused variable in ViewWrapper
    Co-Authored-By: Mark Keller <7525285+keller-mark@users.noreply.github.com>

--- src/ViewWrapper.js
+++ src/ViewWrapper.js
@@ -24,7 +24,6 @@ export default function ViewWrapper(props) {
         multivecTrack,
         onSelectGenomicInterval,
         onViewportRemove,
-        onRequestIntervalTFs,
         drawRegister
     } = props;
     
