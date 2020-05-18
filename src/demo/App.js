ec4ed5668c25282cf72bd9bbd2092d5c72ad2a62
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-05-18 11:35:22 -0400

    Add a nav bar on the top

--- src/demo/App.js
+++ src/demo/App.js
@@ -214,23 +214,49 @@ export default function App() {
 
     return (
         <div className="app">
-            <div className="header">
-                <h4>Cistrome HiGlass Wrapper</h4>
-            </div>
-            <div className="viewconf-options">
-                <select 
-                    onChange={e => setSelectedDemo(e.target.value)} 
-                    defaultValue={selectedDemo}
-                >
-                    {Object.keys(demos).map(vcKey => (
-                        <option 
-                            key={vcKey} 
-                            value={vcKey} 
-                        >
-                            {vcKey}
-                        </option>
-                    ))}
-                </select>
+            <div className="navbar header-container">
+                <div className="header">
+                    <span className="navbar-nav">
+                        <span className="nav-item text-truncate">Cistrome Explorer</span>
+                        <span className="nav-item viewconf-options">
+                            <select 
+                                onChange={e => setSelectedDemo(e.target.value)} 
+                                defaultValue={selectedDemo}
+                            >
+                                {Object.keys(demos).map(vcKey => (
+                                    <option 
+                                        key={vcKey} 
+                                        value={vcKey} 
+                                    >
+                                        {vcKey}
+                                    </option>
+                                ))}
+                            </select>
+                        </span>
+                    </span>
+                    <span className="navbar-nav navbar-right header-info">
+                        <span className="nav-item">
+                            <a className=""
+                                href="http://chgw.gehlenborglab.org/docs/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                    viewBox="0 0 1792 1792">
+                                    <title>Documents</title>
+                                    <path fill="#24292F" d="M1528 1280h-248v248q29-10 41-22l185-185q12-12 22-41zm-280-128h288v-896h-1280v1280h896v-288q0-40 28-68t68-28zm416-928v1024q0 40-20 88t-48 76l-184 184q-28 28-76 48t-88 20h-1024q-40 0-68-28t-28-68v-1344q0-40 28-68t68-28h1344q40 0 68 28t28 68z"/>
+                                </svg>
+                            </a>
+                        </span>
+                        <span className="nav-item">
+                            <a className=""
+                                href="https://github.com/hms-dbmi/cistrome-higlass-wrapper">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24">
+                                    <title>GitHub</title>
+                                    <path fill="#24292F" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+                                </svg>
+                            </a>
+                        </span>
+                    </span>
+                </div>
             </div>
 
             <div className="container">
