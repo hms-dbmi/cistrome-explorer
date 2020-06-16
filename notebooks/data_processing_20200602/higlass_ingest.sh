b1e76994d71f7db4a7529f6c38f283cea04fe21a
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-16 18:21:28 -0400

    Update tileset_uids

--- notebooks/data_processing_20200602/higlass_ingest.sh
+++ notebooks/data_processing_20200602/higlass_ingest.sh
@@ -7,10 +7,11 @@ HIGLASS_SERVER_PATH=$1
 echo "Using higlass-server located at ${HIGLASS_SERVER_PATH}"
 
 for D in $(ls -1 ./data/processed | sort); do
-    if [ ${D: -4} == ".mv5" ]; then
-        echo "Ingesting ${D}"
+    if [ ${D: -13} == ".multires.mv5" ]; then
+        TILESET_UID=${D::${#D}-13}
+        echo "Ingesting $TILESET_UID"
         python $HIGLASS_SERVER_PATH/manage.py ingest_tileset \
-            --uid $D \
+            --uid $TILESET_UID \
             --filename ./data/processed/$D \
             --filetype multivec \
             --datatype matrix
