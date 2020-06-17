59766c86ff55fab79b4b1efd02b49fcf52774caf
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-17 16:35:07 -0400

    Update ingest script

--- notebooks/data_processing_20200602/higlass_ingest.sh
+++ notebooks/data_processing_20200602/higlass_ingest.sh
@@ -10,10 +10,17 @@ for D in $(ls -1 ./data/processed | sort); do
     if [ ${D: -13} == ".multires.mv5" ]; then
         TILESET_UID=${D::${#D}-13}
         echo "Ingesting $TILESET_UID"
+        if [ ${D:0:1} == "H" ]; then
+            # Starts with "Homo_sapiens"
+            ASSEMBLY="hg38"
+        else
+            ASSEMBLY="mm10"
+        fi
+        #python $HIGLASS_SERVER_PATH/manage.py delete_tileset --uuid=$TILESET_UID
         python $HIGLASS_SERVER_PATH/manage.py ingest_tileset \
             --uid $TILESET_UID \
             --filename ./data/processed/$D \
             --filetype multivec \
-            --coordSystem hg38
+            --coordSystem $ASSEMBLY
     fi
 done
