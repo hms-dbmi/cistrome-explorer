b7435cdc57c0314ce0557c25b647072aeafe2e4e
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-23 09:58:08 -0400

    Fix rowinfos keyerror / indexerror bug

--- pipelines/cistrome-to-multivec/src/utils.py
+++ pipelines/cistrome-to-multivec/src/utils.py
@@ -6,57 +6,64 @@ def p2f(x):
             return float(x.strip('%'))
         return float(x)
     return x
+
+def dict_get(d, keys, default):
+    curr_d = d
+    try:
+        for k in keys:
+            curr_d = curr_d[k]
+        return curr_d
+    except KeyError:
+        return default
+    except IndexError:
+        return default
     
 def metadata_json_to_row_info(metadata_json):
-    try:
-        treats_0 = metadata_json["treats"][0]
-    except IndexError:
-        treats_0 = dict()
-    
+
     # Flatten the metadata object
     row_info = {
-        "id": metadata_json.get("id", None),
-        "status": metadata_json.get("status", None),
-        "treats__0__cell_line__name": treats_0.get("cell_line__name", None),
-        "treats__0__cell_type__name": treats_0.get("cell_type__name", None),
-        "treats__0__cell_pop__name": treats_0.get("cell_pop__name", None),
-        "treats__0__disease_state__name": treats_0.get("disease_state__name", None),
-        "treats__0__factor__name": treats_0.get("factor__name", None),
-        "treats__0__is_correcting": treats_0.get("is_correcting", None),
-        "treats__0__link": treats_0.get("link", None),
-        "treats__0__name": treats_0.get("name", None),
-        "treats__0__paper__journal__name": treats_0.get("paper__journal__name", None),
-        "treats__0__paper__lab": treats_0.get("paper__lab", None),
-        "treats__0__paper__pmid": treats_0.get("paper__pmid", None),
-        "treats__0__paper__reference": treats_0.get("paper__reference", None),
-        "treats__0__species__name": treats_0.get("species__name", None),
-        "treats__0__strain__name": treats_0.get("strain__name", None),
-        "treats__0__tissue_type__name": treats_0.get("tissue_type__name", None),
-        "treats__0__unique_id": treats_0.get("unique_id", None),
+        "id": dict_get(metadata_json, ["id"], None),
+        "status": dict_get(metadata_json, ["status"], None),
+        "treats__0__cell_line__name": dict_get(metadata_json, ["treats", 0, "cell_line__name"], None),
+        "treats__0__cell_type__name": dict_get(metadata_json, ["treats", 0, "cell_type__name"], None),
+        "treats__0__cell_pop__name": dict_get(metadata_json, ["treats", 0, "cell_pop__name"], None),
+        "treats__0__disease_state__name": dict_get(metadata_json, ["treats", 0, "disease_state__name"], None),
+        "treats__0__factor__name": dict_get(metadata_json, ["treats", 0, "factor__name"], None),
+        "treats__0__is_correcting": dict_get(metadata_json, ["treats", 0, "is_correcting"], None),
+        "treats__0__link": dict_get(metadata_json, ["treats", 0, "link"], None),
+        "treats__0__name": dict_get(metadata_json, ["treats", 0, "name"], None),
+        "treats__0__paper__journal__name": dict_get(metadata_json, ["treats", 0, "paper__journal__name"], None),
+        "treats__0__paper__lab": dict_get(metadata_json, ["treats", 0, "paper__lab"], None),
+        "treats__0__paper__pmid": dict_get(metadata_json, ["treats", 0, "paper__pmid"], None),
+        "treats__0__paper__reference": dict_get(metadata_json, ["treats", 0, "paper__reference"], None),
+        "treats__0__species__name": dict_get(metadata_json, ["treats", 0, "species__name"], None),
+        "treats__0__strain__name": dict_get(metadata_json, ["treats", 0, "strain__name"], None),
+        "treats__0__tissue_type__name": dict_get(metadata_json, ["treats", 0, "tissue_type__name"], None),
+        "treats__0__unique_id": dict_get(metadata_json, ["treats", 0, "unique_id"], None),
         
-        "qc__judge__map": metadata_json["qc"]["judge"]["map"],
-        "qc__judge__peaks": metadata_json["qc"]["judge"]["peaks"],
-        "qc__judge__fastqc": metadata_json["qc"]["judge"]["fastqc"],
-        "qc__judge__frip": metadata_json["qc"]["judge"]["frip"],
-        "qc__judge__pbc": metadata_json["qc"]["judge"]["pbc"],
-        "qc__judge__motif": metadata_json["qc"]["judge"]["motif_judge"],
-        "qc__judge__dhs": metadata_json["qc"]["judge"]["dhs"],
+        "qc__judge__map": dict_get(metadata_json, ["qc", "judge", "map"], None),
+        "qc__judge__peaks": dict_get(metadata_json, ["qc", "judge", "peaks"], None),
+        "qc__judge__fastqc": dict_get(metadata_json, ["qc", "judge", "fastqc"], None),
+        "qc__judge__frip": dict_get(metadata_json, ["qc", "judge", "frip"], None),
+        "qc__judge__pbc": dict_get(metadata_json, ["qc", "judge", "pbc"], None),
+        "qc__judge__motif": dict_get(metadata_json, ["qc", "judge", "motif_judge"], None),
+        "qc__judge__dhs": dict_get(metadata_json, ["qc", "judge", "dhs"], None),
 
-        "qc__table__treat_number": metadata_json["qc"]["table"]["treat_number"],
-        "qc__table__control_number": metadata_json["qc"]["table"]["control_number"],
-        "qc__table__map__0": p2f(metadata_json["qc"]["table"]["map"][0]),
-        "qc__table__fastqc__0": metadata_json["qc"]["table"]["fastqc"][0],
-        "qc__table__frip__0": p2f(metadata_json["qc"]["table"]["frip"][0]),
-        "qc__table__map_number__0": metadata_json["qc"]["table"]["map_number"][0],
-        "qc__table__pbc__0": p2f(metadata_json["qc"]["table"]["pbc"][0]),
-        "qc__table__motif": metadata_json["qc"]["table"]["motif"],
-        "qc__table__dhs": p2f(metadata_json["qc"]["table"]["dhs"]),
-        "qc__table__raw_number__0": metadata_json["qc"]["table"]["raw_number"][0],
+        "qc__table__treat_number": dict_get(metadata_json, ["qc", "table", "treat_number"], None),
+        "qc__table__control_number": dict_get(metadata_json, ["qc", "table", "control_number"], None),
+        "qc__table__map__0": p2f(dict_get(metadata_json, ["qc", "table", "map", 0], None)),
+        "qc__table__fastqc__0": dict_get(metadata_json, ["qc", "table", "fastqc", 0], None),
+        "qc__table__frip__0": p2f(dict_get(metadata_json, ["qc", "table", "frip", 0], None)),
+        "qc__table__map_number__0": dict_get(metadata_json, ["qc", "table", "map_number", 0], None),
+        "qc__table__pbc__0": p2f(dict_get(metadata_json, ["qc", "table", "pbc", 0], None)),
+        "qc__table__motif": dict_get(metadata_json, ["qc", "table", "motif"], None),
+        "qc__table__dhs": p2f(dict_get(metadata_json, ["qc", "table", "dhs"], None)),
+        "qc__table__raw_number__0": dict_get(metadata_json, ["qc", "table", "raw_number", 0], None),
 
-        "qc__table__meta_orig__intron": metadata_json["qc"]["table"]["meta_orig"]["intron"],
-        "qc__table__meta_orig__inter": metadata_json["qc"]["table"]["meta_orig"]["inter"],
-        "qc__table__meta_orig__exon": metadata_json["qc"]["table"]["meta_orig"]["exon"],
-        "qc__table__meta_orig__promoter": metadata_json["qc"]["table"]["meta_orig"]["promoter"],
+        "qc__table__meta_orig__intron": dict_get(metadata_json, ["qc", "table", "meta_orig", "intron"], None),
+        "qc__table__meta_orig__inter": dict_get(metadata_json, ["qc", "table", "meta_orig", "inter"], None),
+        "qc__table__meta_orig__exon": dict_get(metadata_json, ["qc", "table", "meta_orig", "exon"], None),
+        "qc__table__meta_orig__promoter": dict_get(metadata_json, ["qc", "table", "meta_orig", "promoter"], None),
     }
 
     return row_info
