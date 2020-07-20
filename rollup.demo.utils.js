ef06fb34810494f2c8dad77cf8daf30ceadd62f3
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2020-07-19 21:11:27 -0400

    Upgrade higlass to 1.10.2 (#282)
    * Upgrade higlass to 1.10.2
    
    * Changelog

--- rollup.demo.utils.js
+++ rollup.demo.utils.js
@@ -1,4 +1,4 @@
-const hgVersion = '1.10.1';
+const hgVersion = '1.10.2';
 const peerScripts = {
     'development': `
         <script crossorigin type="text/javascript" src="https://unpkg.com/react@16/umd/react.development.js"></script>
