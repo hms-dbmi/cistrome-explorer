29064dca3a72380394534ccd1665efcf7673cb58
sehilyi <sehilyi@gmail.com>
2022-09-17 12:14:35 -0400

    chore: add qc

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -98,6 +98,9 @@ export const demos = {
             rowInfoAttributes: [
                 {field: "Cell Type", type: "nominal", position: "right", width: 200},
                 {field: "Clustering", type: "tree", position: "right", width: 200},
+                { field: "PEAKS_TOTAL", type: "quantitative", title: 'Total peaks', position: "right", width: 100 },
+                { field: "SEQUENCE_LENGTH", type: "quantitative", title: 'Sequence length', position: "right", width: 100 },
+                { field: "READS_MAPPED", type: "quantitative", title: 'Reads mapped', position: "right", width: 100 },
             ],
             rowAggregate: [],
             rowSort: [
